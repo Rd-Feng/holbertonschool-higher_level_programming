998f14656b46e2e73c10e546111ee3b5e96fa80a
Rui Feng <394@holbertonschool.com>
2018-09-11 19:20:51 +0000

    remove extra space

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
     d *= -1
 else:
     d = number % 10
-print("Last digit of {:d} is ".format(number), d, "and is", end=' ')
+print("Last digit of {:d} is".format(number), d, "and is", end=' ')
 if d > 5:
     print("greater than 5")
 elif d == 0:
-    print("is 0")
+    print("0")
 else:
     print("less than 6 and not 0")
