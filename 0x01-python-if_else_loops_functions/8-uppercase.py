e4552d2d23a95770e3c6ad0976c3fac0ca243956
Rui Feng <394@holbertonschool.com>
2018-09-11 17:23:00 +0000

    init commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            up += chr(ord(i) - 32)
+        else:
+            up += i
+    print(up)
