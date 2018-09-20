31580b86c6148c3e14a5c39e8198b8c30d40b7cd
Rui Feng <394@holbertonschool.com>
2018-09-20 23:57:53 +0000

    check for input type

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not len(roman_string):
+    if not roman_string or type(roman_string) is not str:
         return (0)
     n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for l in roman_string:
-        if l not in n.keys():
-            return (0)
-    # TODO - check if string is a valid number
     num, i, size = 0, 0, len(roman_string)
     while i < size:
-        if i + 1 == size:
-            num += n[roman_string[i]]
-        elif n[roman_string[i]] < n[roman_string[i + 1]]:
+        if i + 1 < size and n[roman_string[i]] < n[roman_string[i + 1]]:
             num += n[roman_string[i + 1]] - n[roman_string[i]]
             i += 1
         else:
