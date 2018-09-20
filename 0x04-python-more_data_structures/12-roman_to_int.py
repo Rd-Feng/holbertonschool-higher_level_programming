1d9411a004688b056e011b92813ec03e3886ab8b
Rui Feng <394@holbertonschool.com>
2018-09-20 23:19:51 +0000

    pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,9 +7,10 @@ n = {'I': 1,
      'D': 500,
      'M': 1000}
 
+
 def roman_to_int(roman_string):
     if not roman_string or not len(roman_string):
-        return 0
+        return (0)
     # TODO - check if string is a valid number
     num, i, size = 0, 0, len(roman_string)
     while i < size:
@@ -23,3 +24,6 @@ def roman_to_int(roman_string):
             num += n[roman_string[i]]
             i += 1
     return (num)
+
+roman_number = "IV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
