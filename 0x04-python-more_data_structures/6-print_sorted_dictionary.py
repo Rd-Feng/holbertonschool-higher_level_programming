1466e5531451d6efd5b1cdcf61d1af15376e506e
Rui Feng <394@holbertonschool.com>
2018-09-20 23:08:10 +0000

    remove None check

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary:
-        for k in sorted(a_dictionary):
-            print("{:s}: {}".format(k, a_dictionary[k]))
+    for k in sorted(a_dictionary):
+        print("{:s}: {}".format(k, a_dictionary[k]))
