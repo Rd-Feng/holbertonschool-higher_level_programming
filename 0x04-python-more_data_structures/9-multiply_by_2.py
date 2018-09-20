81ca292ecb4808c097fca5261ec1c2c45cc01fef
Rui Feng <394@holbertonschool.com>
2018-09-20 23:10:49 +0000

    remove None check

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary:
-        new_d = a_dictionary.copy()
-        for k in new_d.keys():
-            new_d[k] *= 2
+    new_d = a_dictionary.copy()
+    for k in new_d.keys():
+        new_d[k] *= 2
     return (new_d)
