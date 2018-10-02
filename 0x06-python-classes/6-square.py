0452f065664164be742ef9a350f5ea9aa5e6bcf8
Rui Feng <394@holbertonschool.com>
2018-10-02 16:57:43 +0000

    docstring

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """ Square class with integer non-negative size """
+    """ Square class with integer non-negative size and
+    a length 2 tuple position. When the square is printed, its
+    top-left location will be decided by the position attribute"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -11,14 +13,17 @@ class Square:
 
     @property
     def size(self):
+        """ size getter """
         return self.__size
 
     @property
     def position(self):
+        """ position setter """
         return self.__position
 
     @size.setter
     def size(self, value):
+        """ size setter """
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -28,6 +33,7 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """ position setter """
         if ((not isinstance(value, tuple)) or
             (len(value) != 2) or
             (value[0] < 0 or value[1] < 0)):
@@ -35,6 +41,7 @@ class Square:
         self.__position = value
 
     def my_print(self):
+        """ print the square """
         for i in range(self.position[1]):
             print()
         for i in range(self.size):
