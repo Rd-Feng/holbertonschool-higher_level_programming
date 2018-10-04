499e9ae1d27f6ccef7beb0c8f3b3367ceec2f864
Rui Feng <394@holbertonschool.com>
2018-10-04 14:06:35 +0000

    init commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module 5-text_indentation
+
+This module define function text_indentation
+"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of the separators"""
+    separators = ['.', '?', ':']
+    isLeadingSp = True
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for c in text:
+        if c == ' ' and isLeadingSp:
+            continue
+        else:
+            print(c, end='')
+            isLeadingSp = False
+            if c in separators:
+                print("\n")
+                isLeadingSp = True
+    print("\n")
