1548fd0571b5f268cf6a357c1936b77480dfd10f
Rui Feng <394@holbertonschool.com>
2018-10-15 19:06:40 +0000

    remove test code

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,17 +12,3 @@ def add_attribute(obj, name, value):
         setattr(obj, name, value)
     else:
         raise TypeError('can\'t add new attribute')
-
-class MyClass():
-    pass
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
