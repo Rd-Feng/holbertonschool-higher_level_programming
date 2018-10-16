c925c7758d1465c785cecbbcb0430f4527a751a6
Rui Feng <394@holbertonschool.com>
2018-10-16 21:24:07 +0000

    use asterisk notation to unpack lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,10 +5,9 @@
 def read_lines(filename="", nb_lines=0):
     """ reads nb_lines lines of the text file filename """
     with open(filename, encoding='utf-8') as f:
-        lines = f.readlines()
-    if nb_lines <= 0 or nb_lines >= len(lines):
-        for line in lines:
-            print(line, end="")
+        ls = f.readlines()
+    #print(*ls[:nb_lines if nb_lines in range(1, len(ls)) else None], end="")
+    if nb_lines <= 0 or nb_lines >= len(ls):
+        print(*ls, end="")
     else:
-        for i in range(nb_lines):
-            print(lines[i], end="")
+        print(*ls[:nb_lines], end="")
