1b78e24714929ee52de229ed5ff623a46d6fe5bb
Rui Feng <394@holbertonschool.com>
2018-10-17 01:03:45 +0000

    remove sep

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ def read_lines(filename="", nb_lines=0):
     """ reads nb_lines lines of the text file filename """
     with open(filename, encoding='utf-8') as f:
         ls = f.readlines()
-    if nb_lines <= 0 or nb_lines >= len(ls):
-        print(*ls, end="")
-    else:
-        print(*ls[:nb_lines], end="")
+        if nb_lines <= 0 or nb_lines >= len(ls):
+            print(*ls, end="", sep="")
+        else:
+            print(*ls[:nb_lines], end="", sep="")
