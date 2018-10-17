676c64fd9b0e9d02b3c7bb30de3780c613e96c35
Rui Feng <394@holbertonschool.com>
2018-10-17 00:51:54 +0000

    pep8

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,6 @@ def read_lines(filename="", nb_lines=0):
     """ reads nb_lines lines of the text file filename """
     with open(filename, encoding='utf-8') as f:
         ls = f.readlines()
-    #print(*ls[:nb_lines if nb_lines in range(1, len(ls)) else None], end="")
     if nb_lines <= 0 or nb_lines >= len(ls):
         print(*ls, end="")
     else:
