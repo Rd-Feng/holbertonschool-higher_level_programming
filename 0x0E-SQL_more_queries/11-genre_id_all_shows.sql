515f1e54d674d9e678e1bdcc675e02380c4894b9
Rd-Feng <394@holbertonschool.com>
2018-12-04 16:38:58 -0800

    fix: list all tv_shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows that have at least one genre linked
 SELECT shows.title, show_genres.genre_id
 FROM tv_show_genres AS show_genres
-INNER JOIN tv_shows AS shows
+LEFT JOIN tv_shows AS shows
 ON shows.id = show_genres.show_id
 ORDER BY shows.title, show_genres.genre_id;
