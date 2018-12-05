3e864ae03e2fd15299708bfcf739fddb0055715e
Rd-Feng <394@holbertonschool.com>
2018-12-04 16:59:27 -0800

    remove null check

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT shows.title, show_genres.genre_id
 FROM tv_show_genres AS show_genres
 INNER JOIN tv_shows AS shows
-ON shows.id = show_genres.show_id AND genre_id IS NOT NULL
+ON shows.id = show_genres.show_id
 ORDER BY shows.title, show_genres.genre_id;
