b49a1d352556459d4b8710d3a5aa7da303e80a08
Rd-Feng <394@holbertonschool.com>
2019-01-03 07:53:27 -0800

    Fix: make case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""
         SELECT * FROM states
-        WHERE name = '{}'
+        WHERE BINARY name = '{}'
         ORDER BY id;
         """.format(sys.argv[4]))
     for row in cur.fetchall():
