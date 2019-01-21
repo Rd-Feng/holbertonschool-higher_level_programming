6b92f00aad3512b8a28afc796bca26b3b746c6f7
Rd-Feng <394@holbertonschool.com>
2019-01-21 13:37:46 -0800

    print content if no error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     r = requests.get(argv[1])
     try:
         r.raise_for_status()
+        print(r.content.decode(r.encoding))
     except requests.exceptions.HTTPError as e:
-        print(i)
         print("Error code: {}".format(e.response.status_code))
