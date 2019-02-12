04837c108303432e4991dc9086e5d074108559cd
Rd-Feng <394@holbertonschool.com>
2019-02-12 13:12:24 -0800

    semi check

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-module.exports.callMeMoby = function (n, func) {
-  for (let i = 0; i < n; i++) func()
-}
+module.exports.callMeMoby = (n, func) => { for (let i = 0; i < n; i++) func(); };
