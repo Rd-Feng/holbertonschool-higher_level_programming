ed7fe7771fa9e2fa57d8f0787aaa61fbc605101f
Rd-Feng <394@holbertonschool.com>
2019-02-12 14:26:11 -0800

    try ternary

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const occurrences = parseInt(process.argv[2]);
-if (occurrences) {
-  for (let i = 0; i < occurrences; i++) console.log('C is fun');
-} else {
-  console.log('Missing number of occurrences');
-}
+// if (occurrences) {
+//   for (let i = 0; i < occurrences; i++) console.log('C is fun');
+// } else {
+//   console.log('Missing number of occurrences');
+// }
+occurrences ? (function (occurrences) { for (let i = 0; i < occurrences; i++) console.log('C is fun'); })(occurrences) : console.log('missing number');
