321fb00e820eaed44640a45af35af26d547c0349
Rd-Feng <394@holbertonschool.com>
2019-02-18 12:56:14 -0800

    inherit from 5

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-class Square extends require('./4-rectangle') {
-  constructor (size) {
-    super(size, size);
-  }
-}
-Square.prototype.charPrint = function (c = 'X') {
-  for (let i = 0; i < this.height; i++) {
-    console.log(c.repeat(this.width));
+module.exports = class Square extends require('./5-square') {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 };
-module.exports = Square;
