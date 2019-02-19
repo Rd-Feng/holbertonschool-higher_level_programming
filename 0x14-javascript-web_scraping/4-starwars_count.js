8fa8752da7ed8741388981198208c75b6114a486
Rd-Feng <394@holbertonschool.com>
2019-02-19 12:51:57 -0800

    use film endpoint

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-let endPoint = 'https://swapi.co/api/people/18/';
-request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
+const character = 'https://swapi.co/api/people/18/';
+request({ url: process.argv[2], methods: 'GET' }, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    console.log(body && JSON.parse(body).films.length);
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach(film => {
+      if (film.characters.includes(character)) {
+        count++;
+      }
+    });
+    console.log(count);
   }
 });
