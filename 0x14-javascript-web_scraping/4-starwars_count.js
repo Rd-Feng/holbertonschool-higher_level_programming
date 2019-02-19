8316f141b29a84c01dacfadb34989e3923099d23
Rd-Feng <394@holbertonschool.com>
2019-02-19 12:54:08 -0800

    check argument exists

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const character = 'https://swapi.co/api/people/18/';
-request({ url: process.argv[2], methods: 'GET' }, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes(character)) {
-        count++;
-      }
-    });
-    console.log(count);
-  }
-});
+if (process.argv[2] !== undefined) {
+  request({ url: process.argv[2], methods: 'GET' }, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      const films = JSON.parse(body).results;
+      let count = 0;
+      films.forEach(film => {
+        if (film.characters.includes(character)) {
+          count++;
+        }
+      });
+      console.log(count);
+    }
+  });
+}
