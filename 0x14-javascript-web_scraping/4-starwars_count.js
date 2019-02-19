5f6573572469cc8d2d864c5e83d595f59339bbb9
Rd-Feng <394@holbertonschool.com>
2019-02-19 13:04:01 -0800

    testing checker

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const character = 'https://swapi.co/api/people/18/';
-if (process.argv[2] !== undefined) {
-  request.get(process.argv[2], function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      const films = JSON.parse(body).results;
-      let count = 0;
-      films.forEach(film => {
-        if (film.characters.includes(character)) {
-          count++;
-        }
-      });
-      console.log(count);
-    } else {
-      console.log('Error:', response.statusCode);
+request.get(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  else if (response.statusCode === 200) {
+    let count = 0;
+    let films = JSON.parse(body).results;
+    for (let movie of films) {
+      for (let char of movie.characters) {
+        if (char.endsWith('18/')) { count++; }
+      }
     }
-  });
-}
+    console.log(count);
+  }
+});
